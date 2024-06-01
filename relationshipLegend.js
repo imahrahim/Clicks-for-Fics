33a1472052cc5dae03e06ac4e5ddb51863c8428a
imahrahim <imah.rahim@me.com>
2024-06-01 22:48:23 +0200

    legende relationship

--- relationshipLegend.js
+++ relationshipLegend.js
@@ -1,26 +1,62 @@
 export function relationshipLegendSketch(p) {
-    p.setup = function () {
-        let canvas = p.createCanvas(300, 100);
-        canvas.parent(p._userNode); // ensures the canvas is appended to the correct DOM element
-    };
+  p.setup = function () {
+    let canvas = p.createCanvas(300, 300);
+    canvas.parent(p._userNode); // ensures the canvas is appended to the correct DOM element
+  };
 
-    p.draw = function () {
-        p.background(255);
+  p.draw = function () {
+    p.background(255, 200, 200);
 
-        let legends = [
-            { name: "Marvel", color:"#F0519E"}, 
-            { name: "Harry Potter", color: "#589BCF" }, 
-            { name: "Boku No Hero", color: "#87D4A5"}  
-        ];
-    
+    let legends = [
+      { name: "Marvel", color: "#F0519E" },
+      { name: "Harry Potter", color: "#589BCF" },
+      { name: "Boku No Hero", color: "#87D4A5" },
+    ];
+   
+    legends.forEach((legend, index) => {
+        p.textAlign(p.LEFT, p.CENTER);
+      p.fill(legend.color);
+     p.noStroke();
+    //   p.ellipse(10, 10 + index * 30, 5);
+      p.textSize(16);
+      p.textStyle(p.BOLD);
+      p.text(legend.name, 10, 15 + index * 30);
+    });
 
-        legends.forEach((legend, index) => {
-            p.fill(legend.color);
-            p.noStroke();
-            p.ellipse(10, 10 + index * 30, 20);
-            p.fill(0);
-            p.textSize(12);
-            p.text(legend.name, 30, 10 + index * 30);
-        });
-    };
+    p.strokeWeight(1);
+    p.fill("#87D4A5");
+    p.stroke(0);
+    p.rect(10, 100, 200, 20);
+    p.rect(10, 150, 200, 20, 20);
+
+    p.fill("#87d4a486");
+    p.noStroke();
+    p.rect(10, 200, 200, 20);
+
+    p.textFont("Calibri, sans-serif");
+    p.textSize(11);
+    p.fill(0);
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textStyle(p.BOLD)
+    p.text("male", 110, 110);
+    p.textStyle(p.BOLDITALIC)
+    p.text("female", 110, 160);
+
+    p.textStyle(p.ITALIC)
+    p.text("other", 110, 210);
+
+    p.textAlign(p.CENTER, p.CENTER);
+
+    p.text("frequency of", 250, 255);
+    p.text("relationship", 250, 265);
+    p.stroke(0);
+    p.strokeWeight(5);
+    p.line(10, 250, 210, 250);
+
+    p.strokeWeight(2);
+    p.line(10, 260, 210, 260);
+
+    p.strokeWeight(1);
+    p.line(10, 270, 210, 270);
+  };
 }
