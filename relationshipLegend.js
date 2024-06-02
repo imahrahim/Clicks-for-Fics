e949e5136d3ac1872c9069f74da79e8aa92d9db2
imahrahim <imah.rahim@me.com>
2024-06-02 22:32:54 +0200

    relationship legende

--- relationshipLegend.js
+++ relationshipLegend.js
@@ -1,63 +1,87 @@
 export function relationshipLegendSketch(p) {
   p.setup = function () {
-    let canvas = p.createCanvas(300, 300);
+    let canvas = p.createCanvas(300, 400);
     canvas.parent(p._userNode); // ensures the canvas is appended to the correct DOM element
   };
 
   p.draw = function () {
     // p.background(255, 200, 200);
 p.clear();
-    // let legends = [
-    //   { name: "Marvel", color: "#F0519E" },
-    //   { name: "Harry Potter", color: "#589BCF" },
-    //   { name: "Boku No Hero", color: "#87D4A5" },
-    // ];
-   
-    // legends.forEach((legend, index) => {
-    //     p.textAlign(p.LEFT, p.CENTER);
-    //   p.fill(legend.color);
-    //  p.noStroke();
-    // //   p.ellipse(10, 10 + index * 30, 5);
-    //   p.textSize(16);
-    //   p.textStyle(p.BOLD);
-    //   p.text(legend.name, 10, 15 + index * 30);
-    // });
 
     p.rectMode(p.CENTER)
-    p.strokeWeight(1);
+    p.textAlign(p.CENTER, p.CENTER);
+
+    p.strokeWeight(2);
     p.fill("#ffffffc8");
     p.stroke(0);
-    p.rect(150, 100, 200, 20);
-    p.rect(150, 130, 200, 20, 20);
+    p.rect(150, 50, 200, 20);
+    p.rect(150, 80, 200, 20, 20);
 
     p.fill("#ffffff79");
     p.noStroke();
-    p.rect(150, 150, 200, 20);
+    p.rect(150, 110, 200, 20);
 
     p.textFont("Calibri, sans-serif");
     p.textSize(11);
     p.fill(0);
-    p.textAlign(p.CENTER, p.CENTER);
     p.textStyle(p.BOLD)
-    p.text("male", 150, 110);
+    p.text("male", 150, 50);
     p.textStyle(p.BOLDITALIC)
-    p.text("female", 150, 140);
+    p.text("female", 150, 80);
 
     p.textStyle(p.ITALIC)
-    p.text("other", 150,160);
+    p.text("other", 150,110);
 
-    p.textAlign(p.CENTER, p.CENTER);
+    p.fill(255)
+    p.text("frequency of", 150, 155);
+    p.text("characters", 150, 165);
 
-    p.text("frequency of", 250, 255);
-    p.text("relationship", 250, 265);
-    p.stroke(0);
+    p.rectMode(p.CORNER);
+    let w = 300;
+    let xScale = d3.scaleLog()
+    .domain([1, 100 || 1])
+    .range([100, w - 10]);
+    let startX = xScale.domain()[0];
+    let endX = xScale.domain()[1];
+    let step = 10; 
+    let draw = true; 
+
+    for (let i = startX; i < endX; i += step) {
+        let x = xScale(i);
+        let xNext = xScale(i + step);
+        let rectWidth = xNext - x; 
+        
+        if (draw) {
+            p.noStroke();
+            p.fill('#ffffff38')
+            if (x >= 0 && x <= p.width - 10) {
+                p.rect(x, 150, rectWidth, 70); 
+                p.push()
+                p.angleMode(p.DEGREES)
+                p.translate(x+1, 185); 
+                p.rotate(270); 
+                p.fill(255)
+                p.textSize(8)
+                p.text(i-1,0,0)
+                p.pop()
+            }
+        }
+        draw = !draw;
+    }
+
+    p.fill(255)
+    p.textStyle(p.NORMAL)
+    p.text("frequency of", 250, 290);
+    p.text("relationship", 250, 300);
+    p.stroke(255);
     p.strokeWeight(5);
-    p.line(20, 250, 210, 250);
+    p.line(50, 270, 210, 300);
 
     p.strokeWeight(2);
-    p.line(20, 260, 210, 260);
+    p.line(50, 280, 210, 310);
 
     p.strokeWeight(1);
-    p.line(20, 270, 210, 270);
+    p.line(50, 290, 210, 320);
+
   };
 }
