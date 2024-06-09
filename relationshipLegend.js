ff57eba864433a03a33e22546f933a5a442953da
imahrahim <imah.rahim@me.com>
2024-06-09 19:40:05 +0200

    text

--- relationshipLegend.js
+++ relationshipLegend.js
@@ -70,8 +70,10 @@ p.clear();
     p.fill(255)
     p.textStyle(p.NORMAL)
     p.text("gender of character", 150, 20);
-    p.text("frequency of character", 150, 155);
+    p.text("frequency of character = (f) ", 150, 155);
     p.text("frequency of relationships", 150, 280);
+
+
 
     p.stroke(255);
     p.strokeWeight(5);
